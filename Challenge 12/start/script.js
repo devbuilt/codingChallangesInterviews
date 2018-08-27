61b17f95d86a61d0709f8de0c6553afb2cb7edbb
Leonel Tejeda <leonelhtejeda@gmail.com>
2018-08-26 23:14:17 -0700

    completed challange 12

--- Challenge 12/start/script.js
+++ Challenge 12/start/script.js
@@ -10,8 +10,8 @@ var a = [1, 2, 3];
 var b;
 
 //Copy array here
-
-b.push("newElement");
+b = a.slice(); // slice the last element array now equal [1, 2, 3];
+b.push("newElement"); // be now equal [1, 2, 3, 'newElement'];
 
 console.log(a);
 // [1, 2, 3]
